4cfc7a83d0d6e6d3d8966cd414af9e8f3ee3783f
Alexey Zuev <a.zuev@krr.aero>
2022-03-04 15:51:06 +0300

    Custom admin panel 1

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,26 @@
+import datetime
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+from django.utils import timezone
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    date_hierarchy = 'pub_date'
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
